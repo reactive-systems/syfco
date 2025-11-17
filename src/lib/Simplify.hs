reactive-systems/syfco

commit 41c319896a8ead2aa1ae03cd3f2039056b1462cf

its down to evaluate now, it has to build the LTL with Next or Weak and Strong Next based on config and spec

src/lib/Simplify.hs
@@ -175,10 +175,14 @@ simplify c f =
       Not (Triggered x y)
         | ss || nnf                         -> simplify' $ Since (Not x) $ Not y
         | otherwise                        -> Not $ Triggered (simplify' x) $ simplify' y
+      Finally (StrongNext x)
+        | ss || ln || hf                     -> simplify' $ StrongNext $ Finally x
+        | nf || nd                          -> simplify' $ Until TTrue $ StrongNext x
+        | otherwise                        -> Finally $ simplify' $ StrongNext x
       Finally (Next x)
         | ss || ln || hf                     -> simplify' $ Next $ Finally x
         | nf || nd                          -> simplify' $ Until TTrue $ Next x
-        | otherwise                        -> Finally $ simplify' $ Next x
+        | otherwise                        -> Finally $ simplify' $ StrongNext x
       Next (Finally x)
         | (hn && not hf) || (lf && not ln && not ss) -> simplify' $ Finally $ Next x
         | otherwise                        -> Next $ simplify' $ Finally x

src/lib/Data/LTL.hs
@@ -86,6 +86,7 @@ data Formula =
   | And [Formula]
   | Or [Formula]
   | Next Formula
+  | WeakNext Formula
   | StrongNext Formula
   | Previous Formula
   | Globally Formula
@@ -106,6 +107,7 @@ instance Ord Formula where
     (Atomic a, Atomic b)             -> compare a b
     (Not a, Not b)                   -> compare a b
     (Next a, Next b)                 -> compare a b
+    (WeakNext a, WeakNext b)         -> compare a b
     (StrongNext a, StrongNext b)     -> compare a b
     (Previous a, Previous b)         -> compare a b
     (Globally a, Globally b)         -> compare a b
@@ -151,6 +153,7 @@ instance Ord Formula where
         And {}          -> 6
         Or {}           -> 7
         Next {}         -> 8
+        WeakNext {}     -> 8
         StrongNext {}   -> 8
         Previous {}     -> 8
         Globally {}     -> 9
@@ -179,6 +182,7 @@ applySub
 applySub f fml = case fml of
   Not x          -> Not $ f x
   Next x         -> Next $ f x
+  WeakNext x     -> WeakNext $ f x
   StrongNext x   -> StrongNext $ f x
   Previous x     -> Previous $ f x
   Globally x     -> Globally $ f x
@@ -261,6 +265,7 @@ subFormulas fml = case fml of
   Atomic _       -> []
   Not x          -> [x]
   Next x         -> [x]
+  WeakNext x     -> [x]
   StrongNext x   -> [x]
   Previous x     -> [x]
   Globally x     -> [x]
@@ -309,6 +314,7 @@ isBooleanNextFormula fml = case fml of
   And xs       -> all isBooleanNextFormula xs
   Or xs        -> all isBooleanNextFormula xs
   Next x       -> isBooleanFormula x
+  WeakNext x   -> isBooleanFormula x
   StrongNext x -> isBooleanFormula x
   _            -> False
 
@@ -362,8 +368,9 @@ fNot fml = case fml of
   FFalse          -> TTrue
   Atomic x        -> Not $ Atomic x
   Not x           -> x
-  Next x          -> StrongNext $ fNot x
-  StrongNext x    -> Next $ fNot x
+  Next x          -> Next $ fNot x
+  WeakNext x      -> StrongNext $ fNot x
+  StrongNext x    -> WeakNext $ fNot x
   Previous x      -> Previous $ fNot x
   Globally x      -> Finally $ fNot x
   Finally x       -> Globally $ fNot x
@@ -446,6 +453,7 @@ simplePrint fml = case fml of
   Atomic x        -> show x
   Not x           -> '!' : simplePrint x
   Next x          -> 'X' : ' ' : simplePrint x
+  WeakNext x      -> 'X' : ' ' : simplePrint x
   StrongNext x    -> "X[!] " ++ simplePrint x
   Previous x      -> 'Y' : ' ' : simplePrint x
   Globally x      -> 'G' : ' ' : simplePrint x
@@ -480,6 +488,7 @@ pastFormula = \case
   Atomic _        -> False
   Not x           -> pastFormula x
   Next x          -> pastFormula x
+  WeakNext x      -> pastFormula x
   StrongNext x    -> pastFormula x
   Previous {}     -> True
   Globally x      -> pastFormula x

src/lib/Writer/Data.hs
@@ -165,6 +165,7 @@ data OperatorConfig =
   , opImplies :: BinaryOperator
   , opEquiv :: BinaryOperator
   , opNext :: UnaryOperator
+  , opWeakNext :: UnaryOperator
   , opStrongNext :: UnaryOperator
   , opPrevious :: UnaryOperator
   , opFinally :: UnaryOperator

src/lib/Writer/Formats/Acacia.hs
@@ -39,7 +39,8 @@ opConfig = OperatorConfig
   , opImplies      = BinaryOp "->"  3 AssocLeft
   , opEquiv        = BinaryOp "<->" 3 AssocLeft
   , opNext         = UnaryOp  "X"   1
-  , opStrongNext   = UnaryOp  "X"   1
+  , opStrongNext   = UnaryOpUnsupported
+  , opWeakNext     = UnaryOpUnsupported
   , opPrevious     = UnaryOpUnsupported
   , opFinally      = UnaryOp  "F"   1
   , opGlobally     = UnaryOp  "G"   1

src/lib/Writer/Formats/AcaciaSpecs.hs
@@ -41,7 +41,8 @@ opConfig = OperatorConfig
   , opImplies      = BinaryOp "->"  3 AssocLeft
   , opEquiv        = BinaryOp "<->" 3 AssocLeft
   , opNext         = UnaryOp  "X"   1
-  , opStrongNext   = UnaryOp  "X"   1
+  , opStrongNext   = UnaryOpUnsupported
+  , opWeakNext     = UnaryOpUnsupported
   , opPrevious     = UnaryOpUnsupported
   , opFinally      = UnaryOp  "F"   1
   , opGlobally     = UnaryOp  "G"   1

src/lib/Writer/Formats/Basic.hs
@@ -42,7 +42,8 @@ opConfig = OperatorConfig
   , opImplies      = BinaryOp "->"  4 AssocRight
   , opEquiv        = BinaryOp "<->" 4 AssocRight
   , opNext         = UnaryOp  "X"   1
-  , opStrongNext   = UnaryOp  "X"   1
+  , opStrongNext   = UnaryOpUnsupported
+  , opWeakNext     = UnaryOpUnsupported
   , opPrevious     = UnaryOp  "Y"   1
   , opFinally      = UnaryOp  "F"   1
   , opGlobally     = UnaryOp  "G"   1

src/lib/Writer/Formats/Bosy.hs
@@ -44,7 +44,8 @@ opConfig = OperatorConfig
   , opImplies      = BinaryOp "->"  4 AssocRight
   , opEquiv        = BinaryOp "<->" 4 AssocRight
   , opNext         = UnaryOp  "X"   1
-  , opStrongNext   = UnaryOp  "X"   1
+  , opStrongNext   = UnaryOpUnsupported
+  , opWeakNext     = UnaryOpUnsupported
   , opPrevious     = UnaryOpUnsupported
   , opFinally      = UnaryOp  "F"   1
   , opGlobally     = UnaryOp  "G"   1

src/lib/Writer/Formats/Example.hs
@@ -67,7 +67,8 @@ opConfig = OperatorConfig
   , opImplies    = BinaryOp "<fancy implication symbol>" 8 AssocRight
   , opEquiv      = BinaryOp "<fancy equivalence symbol>" 8 AssocLeft
   , opNext       = UnaryOp  "<fancy next symbol>"        2
-  , opStrongNext = UnaryOp  "<fancy next symbol>"        2
+  , opStrongNext = UnaryOpUnsupported
+  , opWeakNext   = UnaryOpUnsupported
   , opFinally    = UnaryOp  "<fancy finally symbol>"     3
   , opGlobally   = UnaryOp  "<fancy globally symbol>"    4
   , opUntil      = BinaryOp "<fancy until symbol>"       5 AssocRight

commit dad0e9b1f45b3826afaddc873018a900f6fcd545

fix incorrect simplification rule
Commit 41c3198 mistakenly corrupted the Simplify function to turn
Simplify(F(X(e)) into F(Simplify(X[!]e)), introducing a strong Next
where there was none.

This reverts that part of the change and fixes #53.

src/lib/Simplify.hs
@@ -188,7 +188,7 @@ simplify c f =
       Finally (Next x)
         | ss || ln || hf                     -> simplify' $ Next $ Finally x
         | nf || nd                          -> simplify' $ Until TTrue $ Next x
-        | otherwise                        -> Finally $ simplify' $ StrongNext x
+        | otherwise                        -> Finally $ simplify' $ Next x
       Next (Finally x)
         | (hn && not hf) || (lf && not ln && not ss) -> simplify' $ Finally $ Next x
         | otherwise                        -> Next $ simplify' $ Finally x